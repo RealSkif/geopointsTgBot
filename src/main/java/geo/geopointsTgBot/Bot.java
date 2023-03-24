407b77ee7427304a0fac64e3efc6641468a85231
ar.kuzmin <ar.kuzmin@yandex.ru>
2023-03-24 15:32:57 +0700

    Here we go

---
+++ src/main/java/geo/geopointsTgBot/Bot.java
@@ -0,0 +1,54 @@
+package geo.geopointsTgBot;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.CopyMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotUsername() {
+        return "GGS_Points_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "6054614033:AAFyCqN0X42aLqczS4zux9m_VCAAsjk6EAM";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        var msg = update.getMessage();
+        var user = msg.getFrom();
+        var id = user.getId();
+
+        sendText(id, msg.getText());
+
+    }
+    public void copyMessage(Long who, Integer msgId){
+        CopyMessage cm = CopyMessage.builder()
+                .fromChatId(who.toString())  //We copy from the user
+                .chatId(who.toString())      //And send it back to him
+                .messageId(msgId)            //Specifying what message
+                .build();
+        try {
+            execute(cm);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void sendText(Long who, String what) {
+        SendMessage sm = SendMessage.builder()
+                .chatId(who.toString()) //Who are we sending a message to
+                .text(what).build();    //Message content
+        try {
+            execute(sm);                        //Actually sending the message
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);      //Any error will be printed here
+        }
+    }
+
+}
--- src/main/java/geo/geopointsTgBot/GeopointsTgBotApplication.java
+++ src/main/java/geo/geopointsTgBot/GeopointsTgBotApplication.java
@@ -2,12 +2,18 @@ package geo.geopointsTgBot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
 public class GeopointsTgBotApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GeopointsTgBotApplication.class, args);
+	public static void main(String[] args) throws TelegramApiException {
+		TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+		Bot bot = new Bot();//We moved this line out of the register method, to access it later
+		botsApi.registerBot(bot);
+		bot.sendText(1234L, "Hello World!");  //The L just turns the Integer into a Long
 	}
 
 }
