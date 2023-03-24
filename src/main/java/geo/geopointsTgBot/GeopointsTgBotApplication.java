d87f74ecf6f2a925fd4fa1b171b126887b242c6f
ar.kuzmin <ar.kuzmin@yandex.ru>
2023-03-24 13:50:36 +0700

    Initial commit

---
+++ src/main/java/geo/geopointsTgBot/GeopointsTgBotApplication.java
@@ -0,0 +1,13 @@
+package geo.geopointsTgBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeopointsTgBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeopointsTgBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/geo/geopointsTgBot/GeopointsTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package geo.geopointsTgBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeopointsTgBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
